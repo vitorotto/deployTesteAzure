b5970016fd91a35b99805e1ce6d4ff330bba7905
vitorotto <vitorotto05@gmail.com>
2025-06-12 21:15:32 -0300

    Especificando porta da azure

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Permite que o Express entenda JSON no corpo da requisição
 app.use(express.json());
